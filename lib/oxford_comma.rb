d6a000aee81583b047e70727076bfd6a5bb1d22b
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 23:26:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  new_string = ""
+  if array.size <= 2 
+    new_string << array.join(" and ")
+  else
+    new_string << array.join(", ")
+    length_from_end = new_string.length - array[-1].length
+    new_string.insert(length_from_end, "and ")
+  end
+  new_string
 end
